05c9256df92055d4bd042579e54f19a1dd65f64f
Azat Sultanov <asultanov@quantumbrains.com>
2025-11-14 12:45:44 +0400

    nvim

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,10 +1,17 @@
 return {
   "saghen/blink.cmp",
-  opts = {
-    keymap = {
+  opts = function(_, opts)
+    -- INSERT MODE / LSP
+    opts.keymap = vim.tbl_deep_extend("force", opts.keymap or {}, {
       ["<C-j>"] = { "select_next", "fallback" },
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<Tab>"] = { "select_and_accept", "fallback" },
-    },
-  },
+    })
+
+    -- CMDLINE MODE (:, /, ? и т.п.)
+    opts.cmdline = opts.cmdline or {}
+    opts.cmdline.keymap = vim.tbl_deep_extend("force", opts.cmdline.keymap or { preset = "cmdline" }, {
+      ["<Tab>"] = { "select_and_accept", "fallback" },
+    })
+  end,
 }
